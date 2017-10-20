94dcbe349766078a15c0a0eb7f36f806ace4a427
Faical Yannick Palingwende Congo <faical.congo@nist.gov>
2017-10-20 11:03:29 -0400

    No notion of base for corr_push. we expect to find .reprozip-trace/config.yml for simplicity.

--- reprozip/reprozip/corrapi.py
+++ reprozip/reprozip/corrapi.py
@@ -34,7 +34,7 @@ def push_to_corr(config_path=None, project_name=None):
                 response, content = put_project(client, server_url, project_name)
                 project = json.loads(content)['content']
 
-            push_record(client, server_url, project, base)
+            push_record(client, server_url, project)
         else:
             print(content)
     else:
